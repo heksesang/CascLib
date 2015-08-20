b91500f2a676e27396a4be9930da5b77c1819339
heksesang <ginnus@outlook.com>
2015-08-21 01:08:17 +0200

    Added copyright notice.

--- CascLib/Casc/Shared/ChunkInfo.hpp
+++ CascLib/Casc/Shared/ChunkInfo.hpp
@@ -1,3 +1,22 @@
+/*
+* Copyright 2014 Gunnar Lilleaasen
+*
+* This file is part of CascLib.
+*
+* CascLib is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* CascLib is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with CascLib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 namespace Casc
@@ -12,14 +31,14 @@ namespace Casc
             typedef std::filebuf::traits_type Traits;
 
             // The offset of the first byte in the decompressed data.
-            typename Traits::off_type begin;
+            Traits::off_type begin;
 
             // The offset of the last byte in the decompressed data.
-            typename Traits::off_type end;
+            Traits::off_type end;
 
             // The offset where the compressed data starts.
             // The base position is this->offset.
-            typename Traits::off_type offset;
+            Traits::off_type offset;
 
             // The size of the compressed data.
             size_t size;
